799dbff35f619bf5118c9d6ee97c5035aa67bc80
ebs-uchimura <uchimura@ebisu-do.jp>
2024-12-26 15:59:47 +0900

    20241226

--- suijin/js/category.js
+++ suijin/js/category.js
@@ -3,12 +3,15 @@ $(function () {
     $("#list-head-sort").change(function () {
         // 選択したURLを変数に格納
         const url = $(this).val();
+        console.log(url);
         // 選択したURLに移動
         window.location.href = url;
     });
-    $("#list-head-category").change(function () {
+    $("#list-head-category2").change(function () {
         // 選択したURLを変数に格納
+        console.log($(this));
         const url = $(this).val();
+        console.log(url);
         // 選択したURLに移動
         window.location.href = url;
     });
